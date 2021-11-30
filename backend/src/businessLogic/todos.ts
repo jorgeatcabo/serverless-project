f3352ca7ef3e87bc8ae542575ef832d65759fa15
El Roble <san_lucas2005@yahoo.com>
2021-11-30 12:19:49 -0700

    Add changes for fixing zip building error

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -5,8 +5,8 @@ import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
 import { createLogger } from '../utils/logger'
 import * as uuid from 'uuid'
-import * as createError from 'http-errors'
-import { parseUserId } from '../auth/utils'
+//import * as createError from 'http-errors'
+//import { parseUserId } from '../auth/utils'
 
 // TODO: Implement businessLogic
 
